66104fd0a884f7a5641cef21be0b5c70845b497c
Bernard Boguszewski <bernard.boguszewski@indexcopernicus.com>
2019-11-30 09:53:19 +0100

    spring security

---
+++ src/main/java/com/sdacademy/javalub15/security/SecurityConfiguration.java
@@ -0,0 +1,35 @@
+package com.sdacademy.javalub15.security;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private PasswordEncoder passwordEncoder;
+
+    public SecurityConfiguration(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder)
+                .withUser("user@mail.com")
+                .password(passwordEncoder.encode("pass"))
+                .roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+       http.csrf().disable()
+               .authorizeRequests()
+               .antMatchers("/registration")
+               .permitAll()
+               .and()
+               .formLogin().loginPage("/login")
+               .permitAll();
+    }
+}
