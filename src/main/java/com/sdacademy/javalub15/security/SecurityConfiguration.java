2f5b6e1c316b2a9017f0a8cea7454d1359b49c58
Bernard Boguszewski <bernard.boguszewski@indexcopernicus.com>
2019-11-30 10:13:38 +0100

    spring security

--- src/main/java/com/sdacademy/javalub15/security/SecurityConfiguration.java
+++ src/main/java/com/sdacademy/javalub15/security/SecurityConfiguration.java
@@ -1,16 +1,23 @@
 package com.sdacademy.javalub15.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public SecurityConfiguration(PasswordEncoder passwordEncoder) {
-        this.passwordEncoder = passwordEncoder;
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Override
